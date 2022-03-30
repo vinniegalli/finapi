3afad1d3e7163198bcec8412f184e854d512b423
Vinicius Galli <vinniegalli@gmail.com>
2022-03-29 22:54:52 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# finapi
+# FIN API
+
+## THIS IS A FINANCIAL API
+
+### REQUIREMENTS 
+
+- [x] Create a account
+- [x] Serch for client's account balance
+- [] Make a transaction
+- [] Search client's account balance by date
+- [] Update client's account
+- [] Request the client's account details
+- [] Delete a account
+
+### BUSINESS RULES 
+
+- [x] Do not create a account with a existing cpf
+- [x] Do not show account balance with a non existing account
+- [] Do not make a transaction with a non existing account
+- [] Do not delete a non existing account
+- [] Do not make a withdraw with a non existing account
---
+++ src/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+
+const {v4: uuidv4} = require('uuid')
+
+const app = express()
+
+app.use(express.json())
+
+const customers = [
+
+]
+
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body
+
+  const customersAlreadyExists = customers.some((customer) => customer.cpf === cpf)
+
+  if(customersAlreadyExists){
+    return response.status(400).json({error: 'Customer already exists'})
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: []
+  })
+
+  return response.sendStatus(201)
+
+})
+
+app.get('/statement', (request, response) => {
+  const { cpf } = request.headers
+
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  if(!customer){
+    return response.status(400).json({error: 'Customer not found'})
+  }
+
+  return response.json(customer.statement)
+})
+
+app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000 🔥' ))
