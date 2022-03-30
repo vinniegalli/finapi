71e7a9dac49fcb8f25e52e976b1f24aa3615d74b
Vinicius Galli <vinniegalli@gmail.com>
2022-03-30 20:49:19 -0300

    Final

--- README.md
+++ README.md
@@ -1,21 +1,19 @@
-# FIN API
-
-## THIS IS A FINANCIAL API
+# FIN API - FINANCIAL
 
 ### REQUIREMENTS 
 
 - [x] Create a account
 - [x] Serch for client's account balance
-- [] Make a transaction
-- [] Search client's account balance by date
-- [] Update client's account
-- [] Request the client's account details
-- [] Delete a account
+- [x] Make a transaction
+- [x] Search client's account balance by date
+- [x] Update client's account
+- [x] Request the client's account details
+- [x] Delete a account
 
 ### BUSINESS RULES 
 
 - [x] Do not create a account with a existing cpf
 - [x] Do not show account balance with a non existing account
-- [] Do not make a transaction with a non existing account
-- [] Do not delete a non existing account
-- [] Do not make a withdraw with a non existing account
+- [x] Do not make a transaction with a non existing account
+- [x] Do not make a withdraw with a non existing account
+- [x] Do not delete a non existing account
--- src/index.js
+++ src/index.js
@@ -10,6 +10,32 @@ const customers = [
 
 ]
 
+function verifyIfExistAccountCpf(request, response, next){
+  const { cpf } = request.headers
+
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  if(!customer){
+    return response.status(400).json({error: 'Customer not found'})
+  }
+
+  request.customer = customer
+
+  return next()
+}
+
+function getBalance(statement){
+  const balance = statement.reduce((acc, operation) => {
+    if(operation.type === 'credit'){
+      return acc + operation.amount
+    } else {
+      return acc - operation.amount
+    }
+  }, 0)
+
+  return balance
+}
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body
 
@@ -30,16 +56,92 @@ app.post('/account', (request, response) => {
 
 })
 
-app.get('/statement', (request, response) => {
-  const { cpf } = request.headers
+app.get('/statement', verifyIfExistAccountCpf, (request, response) => {
+  const {customer} = request
+  return response.json(customer.statement)
+})
 
-  const customer = customers.find(customer => customer.cpf === cpf)
+app.get('/statement/date', verifyIfExistAccountCpf, (request, response) => {
+  const {customer} = request
 
-  if(!customer){
-    return response.status(400).json({error: 'Customer not found'})
+  const {date} = request.query
+
+  const dateFormat = new Date(date + ' 00:00')
+
+  const statement = customer.statement.filter((statement) => 
+  statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+
+  return response.json(statement)
+})
+
+app.post('/deposit', verifyIfExistAccountCpf, (request,response) =>{
+  const {description, amount } = request.body
+
+  const {customer} = request
+
+  const statementOperation = {
+    description, 
+    amount,
+    created_at: new Date(),
+    type: 'credit',
+  }  
+
+  customer.statement.push(statementOperation)
+
+  return response.status(201).send()
+  
+})
+
+app.post('/withdraw', verifyIfExistAccountCpf, (request,response) =>{
+  const {amount} = request.body
+  const {customer} = request
+
+  const balance = getBalance(customer.statement)
+
+  if(balance < amount){
+    return response.status(400).json({error: 'Insufficient funds'})
   }
 
-  return response.json(customer.statement)
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: 'debit',
+  }
+  
+  customer.statement.push(statementOperation)
+
+  return response.status(201).send()
+})
+
+app.put('/account', verifyIfExistAccountCpf, (request,response) =>{
+  const {name} = request.body
+  const {customer} = request
+
+  customer.name = name
+
+  return response.status(201).send()
+})
+
+app.get('/account', verifyIfExistAccountCpf, (request, response) =>{
+  const {customer} = request
+
+  return response.json(customer)
+})
+
+app.delete('/account', verifyIfExistAccountCpf, (request, response) => {
+  const {customer} = request
+
+  customers.splice(customer, 1)
+
+  return response.status(200).json(customers)
+})
+
+app.get('/balance',verifyIfExistAccountCpf, (request,response) => {
+  const {customer} = request
+
+  const balance = getBalance(customer.statement)
+
+  return response.status(200).json(balance)
 })
 
 app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000 🔥' ))
